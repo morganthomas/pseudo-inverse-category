d69d18a148651a7b92b10098e3084066e3961cbc
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2021-05-31 20:37:20 -0400

    initial commit

---
+++ nix/base.nix
@@ -0,0 +1,117 @@
+let
+  f =
+    build-or-shell:
+    { chan ? "5272327b81ed355bbed5659b8d303cf2979b6953"
+    , compiler ? "ghc865"
+    , withHoogle ? false
+    , doHoogle ? false
+    , doHaddock ? false
+    , enableLibraryProfiling ? false
+    , enableExecutableProfiling ? false
+    , strictDeps ? false
+    , isJS ? false
+    , system ? builtins.currentSystem
+    , optimize ? true
+    , shpadoinkle-path ? null
+    }:
+    let
+
+
+      # It's a shpadoinkle day
+      shpadoinkle = if shpadoinkle-path != null then shpadoinkle-path else builtins.fetchGit {
+        url    = https://gitlab.com/platonic/shpadoinkle.git;
+        ref    = "master";
+        rev    = "6107b832be2145738ca0d314546caca4c9114882";
+      };
+
+
+      # Additional ignore patterns to keep the Nix src clean
+      ignorance = [
+        "*.md"
+        "figlet"
+        "*.nix"
+        "*.sh"
+        "*.yml"
+      ];
+
+
+      # Get some utilities
+      inherit (import (shpadoinkle + "/nix/util.nix") { inherit compiler isJS pkgs; }) compilerjs gitignore doCannibalize;
+
+
+      # Build faster by doing less
+      chill = p: (pkgs.haskell.lib.overrideCabal p {
+        inherit enableLibraryProfiling enableExecutableProfiling;
+      }).overrideAttrs (_: {
+        inherit doHoogle doHaddock strictDeps;
+      });
+
+
+      # Overlay containing Shpadoinkle packages, and needed alterations for those packages
+      # as well as optimizations from Reflex Platform
+      shpadoinkle-overlay =
+        import (shpadoinkle + "/nix/overlay.nix") { inherit compiler chan isJS enableLibraryProfiling enableExecutableProfiling; };
+
+
+      # Haskell specific overlay (for you to extend)
+      haskell-overlay = hself: hsuper: {
+        "happy" = pkgs.haskell.lib.dontCheck hsuper.happy;
+      };
+
+
+      # Top level overlay (for you to extend)
+      pseudo-inverse-category-overlay = self: super: {
+        haskell = super.haskell //
+          { packages = super.haskell.packages //
+            { ${compilerjs} = super.haskell.packages.${compilerjs}.override (old: {
+                overrides = super.lib.composeExtensions (old.overrides or (_: _: {})) haskell-overlay;
+              });
+            };
+          };
+        };
+
+
+      # Complete package set with overlays applied
+      pkgs = import
+        (builtins.fetchTarball {
+          url = "https://github.com/NixOS/nixpkgs/archive/${chan}.tar.gz";
+        }) {
+        inherit system;
+        overlays = [
+          shpadoinkle-overlay
+          pseudo-inverse-category-overlay
+        ];
+      };
+
+
+      ghcTools = with pkgs.haskell.packages.${compiler};
+        [ cabal-install
+          ghcid
+        ] ++ (if isJS then [] else [ stylish-haskell ]);
+
+
+      # We can name him George
+      pseudo-inverse-category = pkgs.haskell.packages.${compilerjs}.callCabal2nix "pseudo-inverse-category" (gitignore ignorance ../.) {};
+
+
+    in with pkgs; with lib;
+
+      { build =
+          (if isJS && optimize then doCannibalize else x: x) (chill pseudo-inverse-category);
+
+        shell =
+          pkgs.haskell.packages.${compilerjs}.shellFor {
+            inherit withHoogle;
+            packages    = _: [ pseudo-inverse-category ];
+            COMPILER    = compilerjs;
+            buildInputs = ghcTools;
+            shellHook   = ''
+              ${lolcat}/bin/lolcat ${../figlet}
+              cat ${../intro}
+            '';
+          };
+      }.${build-or-shell};
+in
+  { build = f "build";
+    shell = f "shell";
+  }
